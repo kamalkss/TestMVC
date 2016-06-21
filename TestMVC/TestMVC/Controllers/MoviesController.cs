2eb5f6ed9f4a8023f80f3138f74e0a1311136c47
kamalkss <kamalkss@DESKTOP-B4MCIGP>
2016-06-21 19:21:08 +0430

    taghir

---
+++ TestMVC/TestMVC/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TestMVC.Models;
+
+namespace TestMVC.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            var movie = new Movies();
+            movie.MovieName = "pashmak";
+            return Content(movie.MovieName);
+        }
+    }
+}
---
+++ TestMVC/TestMVC/Models/Movies.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestMVC.Models
+{
+    public class Movies
+    {
+        public int MovieId { get; set; }
+        public string MovieName { get; set; }
+    }
+}
