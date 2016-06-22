3e43f38d49022ac3f8b3201530c79c9213ac2ed9
Mahdi <Mahdi>
2016-06-22 19:07:47 +0430

    MvcApplication 2 sample

---
+++ MvcApplication2/MvcApplication2/Controllers/UsersController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication2.Controllers
+{
+    public class UsersController : Controller
+    {
+        //
+        // GET: /Users/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+
+        [HttpPost,ActionName("Login")]
+        public ActionResult Login_Check(Models.Users_login loginuser)
+        {
+            if (ModelState.IsValid && loginuser.Username == loginuser.Password)
+            {
+                Session.Add("User", loginuser);
+                return RedirectToAction("index", "Default");
+            }
+            return View();
+        }
+
+        
+        public ActionResult Logout()
+        {
+            Session.Remove("User");
+            return RedirectToAction("index", "Default");
+        }
+    }
+}
---
+++ MvcApplication2/MvcApplication2/Models/Users.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcApplication2.Models
+{
+    public class Users_login : IValidatableObject
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public bool RemmeberMe { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (string.IsNullOrEmpty(Username))
+                yield return new ValidationResult("نام کاربری نمیتواند خالی باشد", new[] { "Username" });
+            if (string.IsNullOrEmpty(Password))
+                yield return new ValidationResult("کلمه عبور نمیتواند خالی باشد", new[] { "Password" });
+
+
+            if (Username != Password)
+                yield return new ValidationResult("برای ورود نام کاربری و کلمه عبور باید یکسان باشد", new[] { "Username", "Password" });
+        }
+    }
+}
